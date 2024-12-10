3f475aa74fd6f492498f72a73e7a210a458ee782
chaaax <rodri@LAPTOP-9H0GU95L>
2024-12-10 00:19:28 +0000

    10/12-00:19

--- src/projetofinal/ResultadoEquipa.java
+++ src/projetofinal/ResultadoEquipa.java
@@ -4,10 +4,79 @@
  */
 package projetofinal;
 
+import java.util.List;
+
 /**
  *
  * @author rodri
  */
-public class ResultadoEquipa {
+public class ResultadoEquipa implements CalculavelEstatisticas {
+    private Equipa equipa;
+    private int totalPontos;
+    private int melhorClassificacao;
+    private double tempoTotalEquipa; 
+    
+    
+    
+    public ResultadoEquipa(Equipa equipa, List<ResultadoIndividual> resultadosIndividuais) {
+        this.equipa = equipa;
+        this.melhorClassificacao=melhorClassificacao;
+        this.tempoTotalEquipa=tempoTotalEquipa;
+        calcularEstatisticas(resultadosIndividuais);
+    }
+
+ 
+    
+    @Override
+    public void calcularEstatisticas(List<ResultadoIndividual> resultadosIndividuais) {
+        totalPontos = 0;
+        melhorClassificacao = Integer.MAX_VALUE; // Inicializa como um valor alto
+        tempoTotalEquipa = 0.0;
+
+        for (ResultadoIndividual resultado : resultadosIndividuais) {
+            totalPontos += resultado.getPontos();
+            tempoTotalEquipa += resultado.getTempoTotal();
+            if (resultado.getPosicao() < melhorClassificacao) {
+                melhorClassificacao = resultado.getPosicao();
+            }
+        }
+    }
+    //getters
+
+    public Equipa getEquipa() {
+        return equipa;
+    }
+
+    public int getTotalPontos() {
+        return totalPontos;
+    }
+
+    public int getMelhorClassificacao() {
+        return melhorClassificacao;
+    }
+
+    public double getTempoTotalEquipa() {
+        return tempoTotalEquipa;
+    }
+    
+    //setters
+
+    public void setEquipa(Equipa equipa) {
+        this.equipa = equipa;
+    }
+
+    public void setTotalPontos(int totalPontos) {
+        this.totalPontos = totalPontos;
+    }
+
+    public void setMelhorClassificacao(int melhorClassificacao) {
+        this.melhorClassificacao = melhorClassificacao;
+    }
+
+    public void setTempoTotalEquipa(double tempoTotalEquipa) {
+        this.tempoTotalEquipa = tempoTotalEquipa;
+    }
+    
+    
     
 }
